0a785fb32282c83d3488dc00127a9a9e545f1e37
itair <itair@163.com>
2014-01-05 19:31:15 +0800

    第17章

---
+++ GobalFuc.h
@@ -0,0 +1,12 @@
+//GobalFuc.h //模版重载 与 显示声明  //全局函数
+
+#ifndef TRY0101_TRY0101_GOBLALFUN_H
+#define TRY0101_TRY0101_GOBLALFUN_H
+
+#include <string>
+
+ void showtitle(void);
+
+ bool IsOtto(const std::string & st);
+
+#endif //TRY0101_TRY0101_GOBLALFUN_H
